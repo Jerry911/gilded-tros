ee8f78689671684ad4bc969918ec952001b787c3
Jeroen Pleysier <jeroen.pleysier@xt-i.com>
2025-02-10 15:07:30 +0100

    Added extra unit tests for the backstage passes. Added unit tests for each kind of backstage pass.

--- Java/src/test/java/com/gildedtros/GildedTrosTest.java
+++ Java/src/test/java/com/gildedtros/GildedTrosTest.java
@@ -81,7 +81,7 @@ class GildedTrosTest {
     }
 
     @Test
-    void backstagePasses_increaseInQuality() {
+    void backstagePassesForReFactor_increaseInQuality() {
         Item item = new Item("Backstage passes for Re:Factor", 10, 20);
         GildedTros gildedTros = new GildedTros(new Item[]{item});
 
@@ -92,7 +92,18 @@ class GildedTrosTest {
     }
 
     @Test
-    void backstagePasses_increaseBy3When5DaysOrLess() {
+    void backstagePassesForReFactor_increaseBy2When6To10Days() {
+        Item item = new Item("Backstage passes for Re:Factor", 8, 20);
+        GildedTros gildedTros = new GildedTros(new Item[]{item});
+
+        gildedTros.updateQuality();
+
+        assertEquals(7, item.sellIn);
+        assertEquals(22, item.quality);
+    }
+
+    @Test
+    void backstagePassesForReFactor_increaseBy3When5DaysOrLess() {
         Item item = new Item("Backstage passes for Re:Factor", 5, 20);
         GildedTros gildedTros = new GildedTros(new Item[]{item});
 
@@ -103,8 +114,63 @@ class GildedTrosTest {
     }
 
     @Test
+    void backstagePassesForReFactor_qualityBecomes0WhenSellInIs0OrLess() {
+        Item item = new Item("Backstage passes for Re:Factor", 0, 20);
+        GildedTros gildedTros = new GildedTros(new Item[]{item});
+
+        gildedTros.updateQuality();
+
+        assertEquals(-1, item.sellIn);
+        assertEquals(0, item.quality);
+    }
+
+    @Test
     void backstagePasses_dropToZeroAfterEvent() {
         Item item = new Item("Backstage passes for Re:Factor", 0, 20);
+        GildedTros gildedTros = new GildedTros(new Item[]{item});
+
+        gildedTros.updateQuality();
+
+        assertEquals(-1, item.sellIn);
+        assertEquals(0, item.quality);
+    }
+
+    @Test
+    void backstagePassesForHAXX_increaseInQuality() {
+        Item item = new Item("Backstage passes for HAXX", 10, 20);
+        GildedTros gildedTros = new GildedTros(new Item[]{item});
+
+        gildedTros.updateQuality();
+
+        assertEquals(9, item.sellIn);
+        assertEquals(22, item.quality);
+    }
+
+    @Test
+    void backstagePassesForHAXX_increaseBy2When6To10Days() {
+        Item item = new Item("Backstage passes for HAXX", 8, 20);
+        GildedTros gildedTros = new GildedTros(new Item[]{item});
+
+        gildedTros.updateQuality();
+
+        assertEquals(7, item.sellIn);
+        assertEquals(22, item.quality);
+    }
+
+    @Test
+    void backstagePassesForHAXX_increaseBy3When5DaysOrLess() {
+        Item item = new Item("Backstage passes for HAXX", 5, 20);
+        GildedTros gildedTros = new GildedTros(new Item[]{item});
+
+        gildedTros.updateQuality();
+
+        assertEquals(4, item.sellIn);
+        assertEquals(23, item.quality);
+    }
+
+    @Test
+    void backstagePassesForHAXX_qualityBecomes0WhenSellInIs0OrLess() {
+        Item item = new Item("Backstage passes for HAXX", 0, 20);
         GildedTros gildedTros = new GildedTros(new Item[]{item});
 
         gildedTros.updateQuality();
