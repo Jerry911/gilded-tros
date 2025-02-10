ef89bbfc6b9e5c9da19efbb3a177ed2d78be19eb
Jeroen Pleysier <jeroen.pleysier@xt-i.com>
2025-02-10 15:17:15 +0100

    Updated GildedTRos updateQuality method to make use of simplified loop

--- Java/src/main/java/com/gildedtros/GildedTros.java
+++ Java/src/main/java/com/gildedtros/GildedTros.java
@@ -29,8 +29,7 @@ class GildedTros {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            Item item = items[i];
+        for (Item item : items) {
             ItemUpdater updater = ITEM_UPDATERS.getOrDefault(item.name, new NormalItemUpdater());
             updater.update(item);
         }
